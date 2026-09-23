2e7566e8e828c80bce8c6f2637fbdefb5776ff45
Konstantinos Lemonis <costaslemonis2001@gmail.com>
2026-09-23 21:47:56 +0300

    set4

---
+++ Set4/problem-set-4/section_1.gd
@@ -0,0 +1,7 @@
+extends Node
+
+#1) Declare an array of strings with 5 names in it
+
+#2) Write a function that prints the names in order using a for loop and call it from _ready
+
+#3) Write a function that takes in an array as an argument and swaps the fist value with the last one.
